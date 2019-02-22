e5202fbd80305fc344817bccb5616e0922906d0e
lucho <lucho.2012.tandil@gmail.com>
2019-02-22 16:13:43 -0300

    [DEV] Se agrega controller de pagos

---
+++ API/controllers/PagoController.php
@@ -0,0 +1,28 @@
+<?php
+namespace backend\controllers;
+
+use Yii;
+use yii\filters\VerbFilter;
+
+use backend\controllers\BaseController;
+use common\models\Users;
+
+class PagoController extends BaseController
+{
+    public function actionNuevo()
+    {
+      $id = 'nuevo';
+      $this->getInputData($id);
+
+      //validamos el token
+      if (!isset($this->input['headers']['Authorization'])){ return $this->errorResult( $id, 'token not found' ); }
+      $userM = Users::findOne(['token'=>explode(' ',$this->input['headers']['Authorization'])[1]]);
+      if (count($userM) != 1){ return $this->errorResult( $id, 'user not found' ); }
+
+      
+      $this->salida['error'] = '';
+      $this->salida['result']['success'] = true;
+
+      return $this->successResult($id);
+    }
+}
