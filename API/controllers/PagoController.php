c9260c42cc770618b5c8cd08be5dbeefe0cc3c43
lucho <lucho.2012.tandil@gmail.com>
2019-02-23 20:15:13 -0300

    [DEV] Ya se crean nuevos registros en tabla de pagos

--- API/controllers/PagoController.php
+++ API/controllers/PagoController.php
@@ -5,6 +5,7 @@ use Yii;
 use yii\filters\VerbFilter;
 
 use backend\controllers\BaseController;
+use common\models\Pago;
 use common\models\Users;
 
 class PagoController extends BaseController
@@ -19,7 +20,17 @@ class PagoController extends BaseController
       $userM = Users::findOne(['token'=>explode(' ',$this->input['headers']['Authorization'])[1]]);
       if (count($userM) != 1){ return $this->errorResult( $id, 'user not found' ); }
 
+      //Creamos un nuevo pago
+      $pago              = new Pago();
+      $pago->id_creator  = $userM->id;
+      $pago->amount      = $this->input['data']->monto;
+      $pago->description = $this->input['data']->descripcion;
+      $pago->email       = $this->input['data']->email;
 
+      //Si hay errores los reportamos
+      if( !$pago->create() ){ return $this->errorResult( $id,$pago->errors ); }
+
+      //caso contrario proseguimos
       $this->salida['error'] = '';
       $this->salida['result']['success'] = true;
 
@@ -35,7 +46,6 @@ class PagoController extends BaseController
       if (!isset($this->input['headers']['Authorization'])){ return $this->errorResult( $id, 'token not found' ); }
       $userM = Users::findOne(['token'=>explode(' ',$this->input['headers']['Authorization'])[1]]);
       if (count($userM) != 1){ return $this->errorResult( $id, 'user not found' ); }
-
 
       $this->salida['error'] = '';
       $this->salida['result']['success'] = true;
