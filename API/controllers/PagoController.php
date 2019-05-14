c4f3949fa9e9b003816c82712f9e5a9a9be971da
lucho <lucho.2012.tandil@gmail.com>
2019-05-14 04:58:16 -0300

    Se agregan nuevos campos sobre el medio de pago

--- API/controllers/PagoController.php
+++ API/controllers/PagoController.php
@@ -64,9 +64,31 @@ class PagoController extends BaseController
       ];
       $payment->save();
 
-      $pago                = Pago::findOne(['id'=>$this->input['data']->id]);
-      $pago->status        = $payment->status;
-      $pago->preference_id = $payment->id;
+      $pago                 = Pago::findOne(['id'=>$this->input['data']->id]);
+      $pago->status         = $payment->status;
+      $pago->preference_id  = $payment->id;
+      $pago->operation_type = $payment->operation_type;
+      $pago->status_detail  = $payment->status_detail;
+      $pago->payment_type   = $payment->payment_type;
+      $pago->date_created   = $payment->date_created;
+      $pago->date_last_updated   = $payment->date_last_updated;
+      $pago->date_approved       = $payment->date_approved;
+      $pago->money_release_date  = $payment->money_release_date;
+      $pago->currency_id         = $payment->currency_id;
+      $pago->transaction_amount_refunded = $payment->transaction_amount_refunded;
+      $pago->total_paid_amount   = $payment->transaction_details->total_paid_amount;
+      $pago->finance_charge      = $payment->finance_charge;
+      $pago->net_received_amount = $payment->transaction_details->net_received_amount;
+      $pago->reason              = $payment->reason;
+      $pago->name                = $payment->payer->name;
+      $pago->surname             = $payment->payer->surname;
+      $pago->doc_number          = $payment->payer->identification->number;
+      $pago->doc_type            = $payment->payer->identification->type;
+      $pago->payment_method_id   = $payment->payment_method_id;
+      $pago->payment_type_id     = $payment->payment_type_id;
+      $pago->overpaid_amount     = $payment->transaction_details->overpaid_amount;
+      $pago->fee_details         = json_encode($payment->fee_details);
+
       $pago->save(false);
 
       if ($payment->status != 'approved') { return $this->errorResult( $id, 'Pago rechazado' ); }
